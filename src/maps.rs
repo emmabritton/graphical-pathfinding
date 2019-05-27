b8d0944c602151b28575a8eaadd96a5f9aa2cead
Ray Britton <raybritton@gmail.com>
2019-05-27 08:02:40 +0100

    Add description to maps

--- src/maps.rs
+++ src/maps.rs
@@ -8,6 +8,7 @@ pub struct Map {
     pub start: Coord,
     pub targets: Vec<Coord>,
     pub passable: [[bool; GRID_VERT_COUNT]; GRID_HORZ_COUNT],
+    pub info: String,
 }
 
 pub fn read_map_file(ctx: &mut Context, which: usize) -> Map {
@@ -27,7 +28,10 @@ pub fn read_map_file(ctx: &mut Context, which: usize) -> Map {
                 let mut x = 0_usize;
                 let mut y = 0_usize;
 
-                for letter in buffer.chars() {
+
+                let (map_chars, info_chars) = buffer.split_at(GRID_VERT_COUNT * (GRID_HORZ_COUNT + 1));
+                let chars = map_chars.chars();
+                for letter in chars {
                     match letter {
                         's' => {
                             if start.x == -1 {
@@ -48,9 +52,11 @@ pub fn read_map_file(ctx: &mut Context, which: usize) -> Map {
                         y += 1;
                     }
                     if y > GRID_VERT_COUNT {
-                        panic!("Map {} is too big", which);
+                        break;
                     }
                 }
+
+                let info = info_chars.to_string();
 
                 if start.x < 0 || start.y < 0 {
                     panic!("No start found in map {}", which);
@@ -65,6 +71,7 @@ pub fn read_map_file(ctx: &mut Context, which: usize) -> Map {
                     start,
                     targets,
                     passable,
+                    info,
                 };
             }
             Err(err) => {
